8469230eb3b1e37d0cb3e7a9e1ab72d014d7ad7b
igormaslov1 <maslov.i2000@gmail.com>
2021-03-20 22:08:37 -0400

    This folder is fully front end oriented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './Header/Header.js'
 
 function App() {
+
+    useEffect(() => {
+      fetch("/dftoanalyze").then(response =>
+        response.json().then(data=> {
+          console.log(data)
+        }))
+    }, [])
+    
   return (
     <div className="App">
       <Header/>
